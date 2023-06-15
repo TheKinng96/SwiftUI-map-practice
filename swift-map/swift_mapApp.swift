286bb222c4593b92e39e0d451799a72403bf8dfb
G3N <fengyuan.yap@gmail.com>
2023-06-15 20:55:37 +0900

    Initial Commit

---
+++ swift-map/swift_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_mapApp.swift
+//  swift-map
+//
+//  Created by Feng Yuan Yap on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct swift_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
